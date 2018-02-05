f1f02e21ba5c0453bf071408b7e77e54c169b835
chenchuanwena <wwccw0591@gmail.com>
2018-02-05 18:06:51 +0800

    my-music

--- Data/Conf/conf.php
+++ Data/Conf/conf.php
@@ -7,13 +7,22 @@
  * 系统配文件
  * 所有系统级别的配置
  */;
+//if (!defined('JYMUSIC_VERSION')) exit();
+//define('UC_AUTH_KEY', 'YQO^$hBMd/nVe5A!Xw1vIt`l0DLC4}jm)#_3H]qi'); //加密KE
+//define('DB_TYPE','mysql'); // 数据库类型
+//define('DB_HOST', '121.40.178.43');  // 服务器地址
+//define('DB_NAME','music');  // 数据库名
+//define('DB_USER', 'ccguitar');  // 用户名
+//define('DB_PWD' ,'^&*()67890yuiop');  // 密码
+//define('DB_PORT' ,'3306') ; // 端口
+//define('DB_PREFIX','jy_'); // 数据库表前缀
 if (!defined('JYMUSIC_VERSION')) exit();
 define('UC_AUTH_KEY', 'YQO^$hBMd/nVe5A!Xw1vIt`l0DLC4}jm)#_3H]qi'); //加密KE
 define('DB_TYPE','mysql'); // 数据库类型
-define('DB_HOST', '121.40.178.43');  // 服务器地址
+define('DB_HOST', '127.0.0.1');  // 服务器地址
 define('DB_NAME','music');  // 数据库名
-define('DB_USER', 'ccguitar');  // 用户名
-define('DB_PWD' ,'^&*()67890yuiop');  // 密码
+define('DB_USER', 'root');  // 用户名
+define('DB_PWD' ,'!@#$%12345qwert');  // 密码
 define('DB_PORT' ,'3306') ; // 端口
 define('DB_PREFIX','jy_'); // 数据库表前缀
 	
